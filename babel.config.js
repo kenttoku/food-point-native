cac08e8a47c4bcf3d3d312b630d29407e8227a99
Kent Tokunaga <KentTokunaga@gmail.com>
2019-01-15 19:03:56 -0500

    Setup file structure

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      development: {
+        plugins: [
+          'transform-react-jsx-source',
+          ['module-resolver', {
+            root: ['./src'],
+          }],
+        ],
+      },
+    },
   };
 };
